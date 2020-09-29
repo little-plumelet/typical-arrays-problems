6020e85b49c78089a86511b7a1cc1c1b79e3eca0
Perova <perovaa.24@gmail.com>
2020-09-29 20:07:40 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    let i = 1;
+    if (array === undefined || array.length === 0) return 0;
+    else {
+      let min = array[0];
+      while (i < array.length)
+        {
+          if (min > array[i]) min = array[i];
+          i++;
+        }
+        return min;
+      }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let i = 1;
+  if (array === undefined || array.length === 0) return 0;
+  else {
+    let max = array[0];
+    while (i < array.length)
+      {
+        if (max < array[i]) max = array[i];
+        i++;
+      }
+      return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let i = 0;
+  let average = 0;
+  if (array === undefined || array.length === 0) return 0;
+  else {
+      while (i < array.length) {
+        average += array[i];
+        i++;
+      }
+      return average / array.length;
+    }
 }
